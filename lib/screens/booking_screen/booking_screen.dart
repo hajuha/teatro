14dd4c4a4e07affb14d9c73250243ea0ecf8d043
Hao Hoang <hajuha168@gmail.com>
2021-07-27 05:08:15 +0700

    feat: add booking screen

---
+++ lib/screens/booking_screen/booking_screen.dart
@@ -0,0 +1,47 @@
+import 'package:cinema/screens/booking_screen/custom_app_bar.dart';
+import 'package:cinema/screens/booking_screen/date.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BookingScreen extends StatefulWidget {
+  final String movieName;
+
+  BookingScreen({this.movieName});
+
+  @override
+  _BookingScreenState createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: Column(
+          children: [
+            //app bar
+            CustomAppBar(widget.movieName),
+            DateSelector(),
+
+            //date selector
+            // DateSelector(),
+            //
+            // //Time selector
+            // TimeSelector(),
+            //
+            // //Location and theatre
+            // LocationText(),
+            //
+            // //Seat selector
+            // SeatSelector(),
+            //
+            // //Pay button and seat categories
+            // PayButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
