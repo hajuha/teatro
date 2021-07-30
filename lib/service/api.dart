71bccebba576bbb9e945b14e52a41b164bc18716
Hao Hoang <hajuha168@gmail.com>
2021-07-30 22:32:46 +0700

    change endpoint

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -5,12 +5,12 @@ import 'package:http/http.dart' as http;
 
 class APIService {
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
-    String url = "http://894ab66d92fa.ap.ngrok.io/app-login";
+    String url = "http://aba2fc98d4cc.ap.ngrok.io/app-login";
     final response =
         await http.post(Uri.parse(url), body: requestModel.toJson());
     if (response.statusCode == 201 || response.statusCode == 400) {
       return LoginResponseModel.fromJson(
-        json.decode(response.body),
+        jsonDecode(response.body),
       );
     } else {
       throw Exception('Failed to load data!');
@@ -18,11 +18,11 @@ class APIService {
   }
 
   Future<String> signup(SignupRequestModel requestModel) async {
-    String url = "http://894ab66d92fa.ap.ngrok.io/register";
+    String url = "http://aba2fc98d4cc.ap.ngrok.io/register";
     final response =
         await http.post(Uri.parse(url), body: requestModel.toJson());
     if (response.statusCode == 201 || response.statusCode == 400) {
-      return "success";
+      return "1";
     } else {
       throw Exception('Failed to load data!');
     }
